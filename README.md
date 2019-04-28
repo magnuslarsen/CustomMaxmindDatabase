f41b5ea939ddfd6d26d36042e2f001fe848f9c37
magnuslarsen <>
2019-04-28 08:27:13 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# CustomMaxmindDatabase
+# Custom Maxmind Database
 How to create a custom Maxmind Database, with private IP ranges
+
+I use this in my Elastic-stack installation, your mileage may vary!
+
+# Prerequisites
+On Arch Linux based system:
+```
+$ pacman -S libmaxminddb cpanminus
+```
+
+On Debian based systems:
+```
+$ add-apt-repository ppa:maxmind/ppa
+$ apt update
+$ apt install libmaxminddb0 libmaxminddb-dev mmdb-bin cpanminus
+```
+
+For other Liunx based systems, see [the official repository](https://github.com/maxmind/libmaxminddb)
+
+To install the perl modules, simply run this:
+```
+$ cpanm MaxMind::DB::Common MaxMind::DB::Writer
+```
+
+# Create the database
+Now that the prerequisites are installed, we can start to create the actual database:
+
+Change the `my $file` value to where the database should be created
+
+In the `%internal_networks` you simply just add a key-value pair, in the sample format provided in the script. This replicates the official Maxmind Database format for cities. Additional data can be added, but is not needed for Logstash. Just remember, when adding more data, the `%types` block has to be updated!
+
+To add more languages, just add the [code](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements) to the `%types` block, and to the `language` & `description` keys in the `MaxMind::DB::Writer::Tree->new` block
+
+Once your changes are complete, just run the script:
+```
+# ./create_database.pl
+```
+
+# Logstash configuration
+In your Logstash pipeline configuration, add this to your filter block:
+```
+# Replace [src_ip] with the actual IP-adress field of the document
+if [src_ip] =~ /(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/ {
+  geoip {
+    source   => "[src_ip]"
+    target   => "[geoip]"
+    database => "/etc/logstash/my-database-name.mmdb"
+  }
+}
+else if [src_ip] =~ /^127\./ {
+  # Don't do nothing for localhost
+}
+else {
+  geoip {
+    source => "[src_ip]"
+    target => "[geoip]"
+  }
+}
+```
+This will make all private IP-ranges lookup in your custom database, while all public IP-ranges will use the default Maxmind database
---
+++ create_database.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use MaxMind::DB::Writer::Tree;
+use Net::Works::Network;
+
+my $file = '/etc/logstash/my-database-name.mmdb';
+
+my %types = (
+  accuracy_radius    => 'uint32',
+  city               => 'map',
+  code               => 'utf8_string',
+  continent          => 'map',
+  country            => 'map',
+  dk                 => 'utf8_string',
+  en                 => 'utf8_string',
+  geoname_id         => 'uint32',
+  iso_code           => 'utf8_string',
+  latitude           => 'double',
+  location           => 'map',
+  longitude          => 'double',
+  names              => 'map',
+  time_zone          => 'utf8_string',
+);
+
+my $tree = MaxMind::DB::Writer::Tree->new(
+  ip_version               => 4,
+  record_size              => 24,
+  database_type            => 'my-database-name',
+  languages                => ['en', 'dk'],
+  description              => { en => 'My geoIP database', dk => 'Min geoIP database' },
+  map_key_type_callback    => sub { $types{ $_[0] } },
+  remove_reserved_networks => 0,
+);
+
+
+# The IP-ranges in this list should be sorted by 1) CIDR notation, 2) IP-address! Lowest first:
+# 10.0.0.0/8     => {...}
+# 172.16.0.0/16  => {...}
+# 172.17.0.0/16  => {...}
+# 172.16.0.24/32 => {...}
+#
+# geoname_id can be found here: http://www.geonames.org/2618424/kobenhavn.html (notice 2618424 in the url)
+# longitude and latitude can be found here: https://www.latlong.net/
+my %internal_networks = (
+  # Can be printed as pretty print
+  '192.168.10.0/24' => {
+    city => {
+      geoname_id => 2618424,
+      names      => {
+        en => 'Copenhagen',
+        dk => 'København',
+      },
+    },
+    country => {
+      geoname_id => 2623032,
+      iso_code   => 'DK',
+      names      => {
+        en => 'Denmark',
+        dk => 'Danmark',
+      },
+    },
+    location => {
+      accuracy_radius => 50,
+      latitude        => 55.677191,
+      longitude       => 12.568164,
+      time_zone       => 'Europe/Copenhagen',
+    },
+    continent => {
+      code       => 'EU',
+      geoname_id => 6255148,
+      names      => {
+        en => 'Europe',
+        dk => 'Europa',
+      },
+    },
+  },
+
+  # Or as one line
+  '192.168.11.0/24' => { city => { geoname_id => 2618424, names      => { en => 'Copenhagen', dk => 'København', }, }, country => { geoname_id => 2623032, iso_code   => 'DK', names      => { en => 'Denmark', dk => 'Danmark', }, }, location => { accuracy_radius => 50, latitude        => 55.677191, longitude       => 12.568164, time_zone       => 'Europe/Copenhagen', }, continent => { code       => 'EU', geoname_id => 6255148, names      => { en => 'Europe', dk => 'Europa', }, }, },
+);
+
+for my $address (keys %internal_networks) {
+  my $network = Net::Works::Network->new_from_string(string => $address);
+
+  $tree->insert_network($network, $internal_networks{$address});
+}
+
+open my $fh, '>:raw', $file;
+$tree->write_tree($fh);
+close $fh;
